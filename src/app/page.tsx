0137dad51bc4a4fbadcf60be07a5a83b1768fec4
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-04-30 17:46:29 -0400

    This commit is done to ensure some changes in the page.tsx which is landing page...!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className=" bg-[#081B29]">
       <h1>Home</h1>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
     </div>
   );
 }
