53cf0fd5e40faff073f4f3313c487664fd3294f2
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-05-01 23:46:26 -0400

    page tsx  has been ready!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+'use client';
+import {get, ref} from 'firebase/database';
+import React, { useEffect, useState } from 'react';
+import { database } from '@/app/DbSetUp/firebase';
 
 export default function Home() {
+  
+  const [homeData, setHomeData] = useState([]);
+
+  useEffect(() => {
+    const homeRef = ref(database, 'Home');
+
+    get(homeRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        setHomeData(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error("Error fetching data: ", error);
+    });
+  } , []);
+
+
   return (
     <div className=" bg-[#081B29]">
       <h1>Home</h1>
