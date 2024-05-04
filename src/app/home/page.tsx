d8494ed43e6fbe2b2276c44d51f83582028e49d4
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-05-03 23:51:57 -0400

    Home is having Photo, text and logo

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,39 +1,50 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { db } from '@/app/DbSetUp/firebase';
-import HomeModels from '@/models/homeModels';
+import { ref, get, set } from 'firebase/database';
+import { database } from '@/app/DbSetUp/firebase';
+
+interface HomeData {
+    icon: string;
+    image: string;
+    name: string;
+}
 
 const Home: React.FC = () => {
-    const [homeData, setHomeData] = useState<HomeModels[]>([]);
+    const [homeData, setHomeData] = useState<HomeData | null>(null);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const snapshot = await db.collection('Home').get();
-                const data = snapshot.docs.map(doc => doc.data() as HomeModels);
-                setHomeData(data);
-            } catch (error) {
-                console.log("Error fetching data: ", error);
-            }
-        };
-        fetchData();
+        const homeRef = ref(database, 'home');
 
-        return () => {
-            // Cleanup function if needed
-        };
-    }, []);
+        get(homeRef).then((snapshot) => {
+
+            if (snapshot.exists()) {
+                setHomeData(snapshot.val());
+            }
+            else {
+                console.log("No data available");
+            }
+
+        }).catch((error) => {
+            console.error("Error fetching data: ", error);
+        });
+    }, [])
+
 
     return (
-        <div>
-            <h1>Home</h1>
-            {homeData.map(item => (
-                <div key={item.name}>
-                    <h2>My name is: {item.name}</h2>
-                    <img src={item.photo} alt="This is my photo" />
-                    <img src={item.icon} alt="This is my icon" />
-                    <p>Welcome to the home page</p>
-                </div>
-            ))}
+        <div className="bg-[#081B29]">
+            <h1>Home page main</h1>
+            <div>
+                {homeData && (
+                    <div>
+                        <img className='w-44 h-48' src={homeData.image} alt="name is empty" />
+                        <h2>{homeData.name}</h2>
+                        <img src={homeData.icon} alt="name is empty" />
+
+                    </div>  
+
+                )}
+            </div>
+
         </div>
     );
 };
