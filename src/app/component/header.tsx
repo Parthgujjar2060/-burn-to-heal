c1e6ef55e01dd8097f6fc19eb72b6fbcba90758b
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-05-03 23:51:14 -0400

    Header is having logo now

--- src/app/component/header.tsx
+++ src/app/component/header.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import { ref, get, set } from 'firebase/database';
+import { database } from '@/app/DbSetUp/firebase';
 import Link from 'next/link';
 
-const links = ["Home","About","Avocations","Contact"]; 
+const links = ["Home", "About", "Avocations", "Contact"];
 
-export default function Header() {
+interface HomeData {
+  logo: string;
+}
+
+const Header: React.FC = () => {
+
+  const [headerData, setHeaderData] = React.useState<HomeData | null>(null);
+
+  useEffect(() => {
+    const headerRef = ref(database, 'header');
+
+    get(headerRef).then((snapshot) => {
+
+      if (snapshot.exists()) {
+        setHeaderData(snapshot.val());
+      }
+      else {
+        console.log("No data available");
+      }
+
+    }).catch((error) => {
+      console.error("Error fetching data: ", error);
+    });
+  }, []);
+
+
   return (
-    <nav className="flex justify-center"> 
+    <nav className="flex justify-center">
+      <div>
+        <img src={headerData?.logo} alt="icon" className="w-10 h-10" />
+      </div>
       <ul className="flex justify-around  ">
         {links.map((link) => (
           <li key={link} className="p-4">
@@ -18,3 +49,5 @@ export default function Header() {
     </nav>
   );
 }
+
+export default Header;
