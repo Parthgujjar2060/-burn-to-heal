67120e52db91fa85de55576ca4a5112a23c44e93
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-05-04 21:37:06 -0400

    HOme is getting every data and make it better than previous!!

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { ref, get, set } from 'firebase/database';
+import { ref, get } from 'firebase/database';
 import { database } from '@/app/DbSetUp/firebase';
 
 interface HomeData {
@@ -31,22 +31,53 @@ const Home: React.FC = () => {
 
 
     return (
-        <div className="bg-[#081B29]">
-            <h1>Home page main</h1>
+        <div className="flex items-center justify-center h-screen">
+            <div className='grid grid-cols-1 gap-4'>
+                <h2 className="text-white font-serif">Hello, I am Parth Called</h2>
+                <h1 className="text-4xl text-white font-bold p-1">Arjun</h1>
+                <p className="flex space-x-1 overflow-hidden animate-pulse">
+                    {Array.from("a Full Stack Developer").map((char, i) => (
+                        <span
+                            className={`text-blue-500 animate-reveal delay-${i * 3} text-5xl m-2`}
+                        >{char}
+                        </span>
+                    ))}
+                </p>
+            </div>
+
+            <style>{`
+                @keyframes reveal {
+                    0% {
+                        opacity: 0;
+                        transform: translateY(100%);
+                    }
+                    50% {
+                        opacity: 1;
+                        transform: translateY(0);
+                    }
+                    100% {
+                        opacity: 0;
+                        transform: translateY(-100%);
+                    }
+                }
+
+                .animate-reveal {
+                    animation: reveal 3s infinite;
+                }
+            `}</style>
+
             <div>
                 {homeData && (
                     <div>
-                        <img className='w-44 h-48' src={homeData.image} alt="name is empty" />
-                        <h2>{homeData.name}</h2>
-                        <img src={homeData.icon} alt="name is empty" />
-
-                    </div>  
-
+                        <img className='w-44 h-48 ' src={homeData.image} alt="name is empty" />
+                    </div>
                 )}
             </div>
 
         </div>
     );
+
+    
 };
 
 export default Home;
