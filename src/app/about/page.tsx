9007631623bf0c432580216a7c60f25977940517
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-05-16 23:26:00 -0400

    About page is not starting onaboutpage branch

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div>
             <h1>About</h1>
-            <p>Welcome to the about page</p>
+            <p>This is the about page</p>
         </div>
     );
 };
