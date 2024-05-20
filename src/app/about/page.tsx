2982a1a804644a0e35a2136640761bb28f53b8d8
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-05-19 23:47:56 -0400

    About page is now ready to CSS

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -35,8 +35,6 @@ const About = () => {
 
     return (
         <div>
-            <h1>About</h1>
-            <p>This is the about page</p>
             {data && (
                 <div>
                     <p>{data.paragraph}</p>
