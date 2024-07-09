4a561777c0420f4c3ec2cc6b0bb221fd28d7dcb6
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-07-08 23:19:41 -0400

    home has been changed a lot but height is not getting set under the screen size of 480px

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -23,7 +23,7 @@ const Home: React.FC = () => {
 
     useEffect(() => {
         const homeRef = ref(database, 'home');
-        const linkRef = ref(database, 'home/links');
+        const linkRef = ref(database, 'home/links');  
         const logoRef = ref(database, 'home/icon');
         const imageRef = ref(database, 'home/imageDev');
 
@@ -77,103 +77,96 @@ const Home: React.FC = () => {
     }, []);
 
     return (
-        <div className="homeContainer">
-            <div className="main-container">
-                <div className='flex flex-col justify-center items-center content-container'>
-                    <div className="name">
-                        <h2 className="text-white font-thin">Hello, I am Parth</h2>
-                    </div>
-                    <div className="tagline flex m-5">
-                        <p className="taglinewords flex space-x-1 overflow-hidden animate-pulse">
-                            {Array.from("a Full Stack Developer").map((char, i) => (
-                                <span
-                                    key={i}
-                                    className={`text-blue-500 animate-reveal delay-${i * 3} m-3`}
-                                >{char}
-                                </span>
-                            ))}
-                        </p>
-                    </div>
-                    <div>
-                        <p className="paragraph">
-                            Full-Stack Developer with ability to learn and collaborate
-                            in rapidly changing environments and compositions.
-                        </p>
-                    </div>
-                    <div className="grid grid-cols-2 mt-5" style={{ gap: "140px" }}>
+        <div className="main">
+            <div className="flex items-center h- justify-center">
+                <div className="main-container grid grid-cols-2 gap-4 w-full ml-10">
+                    <div className=' flex flex-col justify-center items-center' style={{ width: '100%', height: '100%' }}>
+                        <div className="name mr-60">
+                            <h2 className="text-white font-thin text-2xl ">Hello, I am Parth</h2>
+                        </div>
+                        <div className="tagline flex m-5">
+                            <p className=" taglinewords flex space-x-1 overflow-hidden animate-pulse">
+                                {Array.from("a Full Stack Developer").map((char, i) => (
+                                    <span
+                                        key={i}
+                                        className={`text-blue-500 animate-reveal delay-${i * 3} text-4xl m-3`}
+                                    >{char}
+                                    </span>
+                                ))}
+                            </p>
+                        </div>
+
                         <div>
-                            <Link href="/contact">
-                                <button className="bg-blue-500 p-2 rounded-lg text-white hover:opacity-50 transition duration-500 ease-in-out">Let's Talk</button>
-                            </Link>
+                            <p className="paragraph w-96" style={{ width: "410px" }}>
+                                Full-Stack Developer with ability to learn and collaborate
+                                in rapidly changing enviroments and compositions.
+                            </p>
                         </div>
-                        <div className="flex gap-2">
-                            {links && Object.entries(links).map(([platform, url]) => (
-                                <div key={platform}>
-                                    <a href={url} target="_blank">
-                                        <img src={url} alt={platform} className="w-10 h-10 hover:opacity-50 cursor-pointer transition duration-500" />
-                                    </a>
-                                </div>
-                            ))}
+                        <div className="grid grid-cols-2 mt-5" style={{ gap: "140px" }}>
+                            <div className="">
+                                <Link href="/contact">
+                                    <button className="bg-blue-500 p-2 rounded-lg text-white hover:opacity-50 transition duration-500 ease-in-out">Let&apos;s Talk</button>
+                                </Link>
+                            </div>
+                            <div className="flex gap-2">
+                                {links && Object.entries(links).map(([platform, url]) => (
+                                    <div key={platform} className="">
+                                        <a href={url} target="_blank">
+                                            <img src={url} alt={platform} className="w-10 h-10 hover:opacity-50 cursor-pointer transition duration-500" />
+                                        </a>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="flex justify-center items-center">
-                    {homeData && (
-                        <div className="flex justify-center items-center">
-                            <div className="w-full h-full border-3 border-blue-500 overflow-hidden">
-                                <img className="parthimage" src={homeData.image} alt="Name is empty" style={{ objectFit: 'cover' }} />
+                    <div className="flex justify-center items-center">
+                        {homeData && (
+                            <div className="flex justify-center items-center">
+                                <div className="w-full h-full border-3 border-blue-500 overflow-hidden">
+                                    <img className="parthimage" src={homeData.image} alt="Name is empty" style={{ objectFit: 'cover' }} />
+                                </div>
                             </div>
+                        )}
+                    </div>
+
+                </div>
+                <div className="flex justify-center items-center w-24">
+                    {homeData && (
+                        <div className="devimage-container" >
+                            <img className="devimage" src={homeData.imageDev} alt="Developer image" />
                         </div>
                     )}
                 </div>
+                {logo && Object.keys(logo).map((key, index) => {
+                    const logoData = logo[key] as HomeData;
+                    const position = getSpecificPosition(index);
+                    const logoClassName = `logo-${index}`;
+                    return (
+                        <a key={key} target="_blank" className={`logo ${logoClassName}`} style={{ left: position.left, top: position.top }}>
+                            <img src={logoData.logo} alt={`Icon ${key}`} />
+                        </a>
+                    );
+                })}
+
             </div>
-            {logo && Object.keys(logo).map((key, index) => {
-                const logoData = logo[key] as HomeData;
-                const position = getSpecificPosition(index);
-                const logoClassName = `logo-${index}`;
-                return (
-                    <a key={key} target="_blank" className={`logo ${logoClassName}`} style={{ left: position.left, top: position.top }}>
-                        <img src={logoData.logo} alt={`Icon ${key}`} />
-                    </a>
-                );
-            })}
+
             <style>{`
-                html, body, #__next, .homeContainer {
-                    height: 100%;
-                    margin: 0;
-                    padding: 0;
-                    overflow: hidden;
+                .home-container {
+                    position: relative;
                 }
 
-                .homeContainer {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                }
-
-                .main-container {
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100%;
-                }
-
-                .content-container {
-                    width: 100%;
-                    height: 100%;
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    justify-content: center;
-                }
-
-                .logo {
+                .logo{
                     position: absolute;
                     opacity: 0.4;
                     z-index: -1;
                 }
-
+    
+                .logo {
+                    position: absolute;
+                    opacity: 0.4; 
+ 
+                }
+    
                 @keyframes reveal {
                     0% {
                         opacity: 0;
@@ -188,7 +181,7 @@ const Home: React.FC = () => {
                         transform: translateY(-100%);
                     }
                 }
-
+    
                 .animate-reveal {
                     animation: reveal 3s infinite;
                 }
@@ -197,6 +190,7 @@ const Home: React.FC = () => {
     );
 
     function getSpecificPosition(index: number) {
+
         switch (index) {
             case 0:
                 return { left: '200px', top: '150px' };  // first line 1st image 
