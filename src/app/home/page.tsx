cccad80bbe8a56bb77c9423162c1be54a63819a0
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-07-09 23:46:32 -0400

    home page set up is perfect

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -77,8 +77,8 @@ const Home: React.FC = () => {
     }, []);
 
     return (
-        <div className="main">
-            <div className="flex items-center h- justify-center">
+        <div className="">
+            <div className="flex items-center pb-28 h-screen">
                 <div className="main-container grid grid-cols-2 gap-4 w-full ml-10">
                     <div className=' flex flex-col justify-center items-center' style={{ width: '100%', height: '100%' }}>
                         <div className="name mr-60">
