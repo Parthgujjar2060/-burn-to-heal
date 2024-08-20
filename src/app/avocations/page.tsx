056d546e846b771c5436a94022da137df9e40465
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-08-19 23:44:24 -0400

    Avocation branch has been initialized

--- src/app/avocations/page.tsx
+++ src/app/avocations/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 interface AvocationsProps {
-    
+
 }
 
 const Avocations: React.FC<AvocationsProps> = () => {
     return (
         <div>
+            {/* Here I have to start the avocatioins page */}
             <h1>Avocations</h1>
             <p>Welcome to the avocations page</p>
         </div>
