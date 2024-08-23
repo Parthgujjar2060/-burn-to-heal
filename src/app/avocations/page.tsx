9faaec93d2cb2e9d654293b8eccd773d9a144bf7
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-08-22 23:58:32 -0400

    Grid has been set in column phase

--- src/app/avocations/page.tsx
+++ src/app/avocations/page.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import AvocationComp from "../component/avocationComp"
+import AvocationComp from "../component/avocationComp";
 
 const Avocations: React.FC = () => {
     return (
         <div>
             <h1>Avocations</h1>
             <p>Welcome to the avocations page</p>
-            
-            <div style={{ display: 'flex', gap: '16px' }}>
-                <AvocationComp 
-                    avocation="Volley ball" 
-                    description="Exploring different genres and expanding knowledge." 
-                    icon={<span role="img" aria-label="books">📚</span>} 
+
+            <div style={{ display: 'grid', gap: '10px' }}>
+                <AvocationComp
+                    avocation="Volley ball"
+                    description="Exploring different genres and expanding knowledge."
+                    icon={<span role="img" aria-label="books">📚</span>}
                 />
-                <AvocationComp 
-                    avocation="Foddieeeeeee" 
-                    description="Discovering new places and cultures." 
-                    icon={<span role="img" aria-label="travel">✈️</span>} 
+                <AvocationComp
+                    avocation="Foddieeeeeee"
+                    description="Discovering new places and cultures."
+                    icon={<span role="img" aria-label="travel">✈️</span>}
                 />
-                <AvocationComp 
-                    avocation="Gaming" 
-                    description="Capturing moments through the lens." 
-                    icon={<span role="img" aria-label="camera">📷</span>} 
+                <AvocationComp
+                    avocation="Gaming"
+                    description="Capturing moments through the lens."
+                    icon={<span role="img" aria-label="camera">📷</span>}
                 />
             </div>
         </div>
