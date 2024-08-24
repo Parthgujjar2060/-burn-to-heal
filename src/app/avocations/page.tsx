37aa56c448b5cdb7242539eb962b550162f0e9c1
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-08-23 23:54:12 -0400

    Database has been imported

--- src/app/avocations/page.tsx
+++ src/app/avocations/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AvocationComp from "../component/avocationComp";
+import { ref, get } from 'firebase/database';
+import { database } from '@/app/DbSetUp/firebase';
 
 const Avocations: React.FC = () => {
+
+    interface avocationData {
+        description: string;
+        icon: React.ReactNode;
+    }
+
+    useEffect(() => {
+        const avocationRef = ref(database, 'avocation');
+
+        get(avocationRef).then((snapshot) => {
+            if (snapshot.exists()) {
+                const data: avocationData = snapshot.val();
+                console.log(data);
+            } else {
+                console.log("No data available");
+            }
+        }).catch((error) => {
+            console.error("Error fetching data: ", error);
+        });
+
+    }, []);
+
     return (
         <div>
 
@@ -11,17 +35,17 @@ const Avocations: React.FC = () => {
             <div style={{ display: 'grid', gap: '10px' }}>
                 <AvocationComp
                     avocation="Volley ball"
-                    description="Exploring different genres and expanding knowledge."
+                    description= ""
                     icon={<span role="img" aria-label="books">📚</span>}
                 />
                 <AvocationComp
                     avocation="Foddieeeeeee"
-                    description="Discovering new places and cultures."
+                    description=""
                     icon={<span role="img" aria-label="travel">✈️</span>}
                 />
                 <AvocationComp
                     avocation="Gaming"
-                    description="Capturing moments through the lens."
+                    description=""
                     icon={<span role="img" aria-label="camera">📷</span>}
                 />
             </div>
