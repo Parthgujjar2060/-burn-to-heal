7563abc504e5db98390ac1759fb94d0760be9214
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-10-02 22:29:40 -0400

    Avocation-component has been updated in terms of height width of header and description

--- src/app/component/avocationComp.tsx
+++ src/app/component/avocationComp.tsx
@@ -8,7 +8,7 @@ interface AvocationCompProps {
 
 const AvocationComp: React.FC<AvocationCompProps> = ({ avocation, description, icon }) => {
     return (
-        <div style={{ border: '1px solid #ddd', borderRadius: '8px', padding: '16px', maxWidth: '300px', height: '39vh' }}>
+        <div className='' style={{ border: '1px solid #ddd', borderRadius: '8px', padding: '20px', maxWidth: '400px', height: '39vh' }}>
             <div style={{ display: 'flex', alignItems: 'center' }}>
                 {icon && <div style={{ marginRight: '12px' }}>{icon}</div>}
                 <h3 style={{ margin: 0 }}>{avocation}</h3>
