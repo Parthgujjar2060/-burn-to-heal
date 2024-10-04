c6f7355880755a25a03d82a7469658dc2f62ed43
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-10-03 23:02:54 -0400

    Div has been added to component

--- src/app/component/avocationComp.tsx
+++ src/app/component/avocationComp.tsx
@@ -8,12 +8,14 @@ interface AvocationCompProps {
 
 const AvocationComp: React.FC<AvocationCompProps> = ({ avocation, description, icon }) => {
     return (
-        <div className='' style={{ border: '1px solid #ddd', borderRadius: '8px', padding: '20px', maxWidth: '400px', height: '39vh' }}>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ border: '1px solid #ddd', borderRadius: '8px', padding: '20px', maxWidth: '400px', height: '39vh' }}>
+            <div  className='header' style={{ display: 'flex', alignItems: 'center' }}>
                 {icon && <div style={{ marginRight: '12px' }}>{icon}</div>}
                 <h3 style={{ margin: 0 }}>{avocation}</h3>
             </div>
+            <div className='description'>
             {description && <p style={{ marginTop: '8px', color: '#555' }}>{description}</p>}
+            </div>
         </div>
     );
 };
