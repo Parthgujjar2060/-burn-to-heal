c0b78b66fb9bd813ae17beda94c1ce1a21480c0a
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-10-12 23:58:10 -0400

    Classname has been added for component width height changes

--- src/app/component/avocationComp.tsx
+++ src/app/component/avocationComp.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../component/avocationComp.css';
 
 interface AvocationCompProps {
     avocation: string;
@@ -8,7 +9,7 @@ interface AvocationCompProps {
 
 const AvocationComp: React.FC<AvocationCompProps> = ({ avocation, description, icon }) => {
     return (
-        <div style={{ border: '1px solid #ddd', borderRadius: '8px', padding: '20px', maxWidth: '400px', height: '39vh' }}>
+        <div className='avocationBox' style={{ border: '1px solid #ddd', borderRadius: '8px', padding: '20px', maxWidth: '400px', height: '39vh' }}>
             <div  className='header' style={{ display: 'flex', alignItems: 'center' }}>
                 {icon && <div style={{ marginRight: '12px' }}>{icon}</div>}
                 <h3 style={{ margin: 0 }}>{avocation}</h3>
