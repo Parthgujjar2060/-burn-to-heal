3da467d1cd83864edb8902c0b95bef5b3efafcbc
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-10-21 23:57:35 -0400

    Component layout has been updated

--- src/app/component/avocationComp.tsx
+++ src/app/component/avocationComp.tsx
@@ -9,13 +9,13 @@ interface AvocationCompProps {
 
 const AvocationComp: React.FC<AvocationCompProps> = ({ avocation, description, icon }) => {
     return (
-        <div className='avocationBox' style={{ border: '1px solid #ddd', borderRadius: '8px', padding: '20px', maxWidth: '400px', height: '39vh' }}>
-            <div  className='header' style={{ display: 'flex', alignItems: 'center' }}>
-                {icon && <div style={{ marginRight: '12px' }}>{icon}</div>}
-                <h3 style={{ margin: 0 }}>{avocation}</h3>
+        <div className='avocationBox'>
+            <div className='header'>
+                {icon && <div className="icon">{icon}</div>}
+                <h3>{avocation}</h3>
             </div>
             <div className='description'>
-            {description && <p style={{ marginTop: '8px', color: '#555' }}>{description}</p>}
+                {description && <p>{description}</p>}
             </div>
         </div>
     );
